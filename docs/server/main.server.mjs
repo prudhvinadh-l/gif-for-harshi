36c613de0974fae7e0803f06730399371e64382d
Prudhvinadh-l <prudhvinadh258@gmail.com>
2024-06-21 23:48:26 +0530

    modified few things

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-I45Y5TM5.mjs";import"./chunk-GKSAMXR6.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-RSENILKJ.mjs";import"./chunk-GKSAMXR6.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
